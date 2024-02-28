ebd053116db9576b0de46a12ca6c4ff2e93f70fc
Njinju Fogap <andrewfogap@gmail.com>
2024-02-28 11:19:24 +0100

    adding methods to extract data from word and pdf cv

--- src/extraction.py
+++ src/extraction.py
@@ -1,1 +1,25 @@
 import utilities
+import os
+
+DATA_DIR = utilities.get_data_directory_path()
+
+
+def extraction_cv(filepath):
+    """
+        This function is used to extract from the csv or word document.
+    """
+    if filepath.lower().endswith('.docx'):
+        # this return the extracted data in type string.
+        return utilities.word_extractor(filepath)
+    else:
+        return utilities.pdf_extractor(filepath)
+
+
+nameofcv = input("Enter name of CV (PDF at the end): ")
+
+filepath = os.path.join(DATA_DIR, "cvs", nameofcv)
+
+print(filepath)
+extracted_text = extraction_cv(filepath)
+
+print("here : ", extracted_text)
